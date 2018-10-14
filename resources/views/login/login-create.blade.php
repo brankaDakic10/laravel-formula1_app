d0e75c8bd487e6fd90d6b30fdf84a9bdee9f02df
brankaDakic10 <branka.dakic@yahoo.com>
2018-10-14 16:33:50 +0200

    Added errors to the login-create.blade

--- resources/views/login/login-create.blade.php
+++ resources/views/login/login-create.blade.php
@@ -22,6 +22,17 @@ Login
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
+    @if (count($errors->all()) > 0)
+
+@foreach($errors->all() as $error)
+    <div class="form-group">
+        <div class="alert alert-danger">
+            <li>{{ $error }}</li>
+        </div>
+    </div>
+@endforeach
+
+@endif
 
 </div>
 @endsection
